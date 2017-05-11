49f3c8ff6fba1a11a095f7ef40ebbe59be676784
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-11 16:34:00 +0900

    Create customers table

---
+++ db/migrate/20170511073027_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :customers do |t|
+      t.string :name, null: false
+      t.string :plan, default: "A"
+      t.text :remarks
+      t.boolean :confirmed, null: false, default: false
+      t.boolean :approved, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
