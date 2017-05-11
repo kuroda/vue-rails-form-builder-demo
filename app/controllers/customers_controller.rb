b8679ef9d6d44a25e5f5a84c6b1f4a2681882148
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-11 16:55:46 +0900

    Add customers controller (index, new, edit, ...)

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,37 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.order(:id)
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to :customers
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.assign_attributes(customer_params)
+    if @customer.save
+      redirect_to :customers
+    else
+      render action: "edit"
+    end
+  end
+
+  private def customer_params
+    params.require(:customer).permit(
+      :name, :plan, :confirmed, :approved, :remarks)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
+
+  resources :customers, except: [ :show, :destroy ]
 end
