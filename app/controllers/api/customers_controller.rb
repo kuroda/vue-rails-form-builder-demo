d5aecc08182ec75c2770fd9220f88202d0eb5bef
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-27 20:57:05 +0900

    Allow user to change template by clicking on A tag

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -3,4 +3,9 @@ class Api::CustomersController < ApplicationController
     @customers = Customer.order(:id)
     render action: "index", layout: false
   end
+
+  def new
+    @customer = Customer.new
+    render action: "new", layout: false
+  end
 end
--- app/javascript/packs/top/spa.js
+++ app/javascript/packs/top/spa.js
@@ -7,28 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      template: "<div>Loading...</div>"
+      template: "<div>Loading...</div>",
+      path: undefined
     },
     render: function(h) {
       return h(this.dynamicComponent)
     },
     computed: {
       dynamicComponent: function() {
+        let self = this
         return {
           template: this.template,
-          data: function() { return {} }
+          data: function() {
+            return {
+              path: undefined,
+              open: false,
+              customer: {
+                name: "",
+                plan: undefined,
+                confirmed: undefined,
+                approved: undefined,
+                remarks: ""
+              }
+            }
+          },
+          watch: {
+            path: function(val, _oldVal) {
+              self.path = val
+            }
+          }
         }
       }
     },
     mounted: function() {
-      let self = this
-      Axios.get("/api/customers")
-        .then(function(response) {
-          self.template = response.data
-        })
-        .catch(function(error) {
-          console.log(error)
-        })
+      this.path = "/api/customers"
+    },
+    watch: {
+      path: function(val, _oldVal) {
+        let self = this
+        Axios.get(val)
+          .then(function(response) {
+            self.template = response.data
+          })
+          .catch(function(error) {
+            console.log(error)
+          })
+      }
     }
   })
 })
