f9ec30d0e8c71970072dfbf187fec974ada1f278
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-28 11:38:33 +0900

    Introduce vue-remote-template

--- app/javascript/packs/top/spa.js
+++ app/javascript/packs/top/spa.js
@@ -1,5 +1,5 @@
 import Vue from "vue/dist/vue.esm"
-import VueRemoteTemplate from "../../vue-remote-template"
+import VueRemoteTemplate from "vue-remote-template"
 
 Vue.config.productionTip = false
 let Comp = Vue.extend({ mixins: [ VueRemoteTemplate ] })
