0e2310fad78a04dc40578bacfa7f6a00d0d5219a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-28 13:44:20 +0900

    Get document title from the remote template

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -1,11 +1,13 @@
 class Api::CustomersController < ApplicationController
   def index
     @customers = Customer.order(:id)
+    response.headers['document-title'] = "List of customers"
     render action: "index", layout: false
   end
 
   def new
     @customer = Customer.new
+    response.headers['document-title'] = "New customer"
     render action: "new", layout: false
   end
 
