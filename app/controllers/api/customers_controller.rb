02a1c53c35bbb0ceadf75cd5586bbc2f85aa29ea
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-28 14:00:00 +0900

    Set layout false on Api::CustomersController

--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -1,18 +1,17 @@
 class Api::CustomersController < ApplicationController
+  layout false
+
   def index
     @customers = Customer.order(:id)
     response.headers['document-title'] = "List of customers"
-    render action: "index", layout: false
   end
 
   def new
     @customer = Customer.new
     response.headers['document-title'] = "New customer"
-    render action: "new", layout: false
   end
 
   def edit
     @customer = Customer.find(params[:id])
-    render action: "edit", layout: false
   end
 end
